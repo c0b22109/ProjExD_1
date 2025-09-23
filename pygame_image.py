a40029eebcc4d6b6bbaf43c595a86d7c5c384428
C0B22109 <c0b2210962@edu.teu.ac.jp>
2025-09-23 16:35:41 +0900

    演習課題1 こうかとんが風の影響を受けるように変更

--- pygame_image.py
+++ pygame_image.py
@@ -21,6 +21,7 @@ def main():
             if event.type == pg.QUIT: return
         
         x = tmr % 3200
+        kk_rct.move_ip((-1, 0))
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
             kk_rct.move_ip((0, -1))
@@ -29,7 +30,7 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1, 0))
+            kk_rct.move_ip((2, 0))
 
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img_fliped, [1600 - x, 0])
