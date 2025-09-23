d605ba34f8222ebca6abd78f796487c17b472ebf
C0B22109 <c0b2210962@edu.teu.ac.jp>
2025-09-23 15:52:42 +0900

    練習問題7 800フレーム後に背景画像をもう1枚配置

--- pygame_image.py
+++ pygame_image.py
@@ -19,6 +19,8 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [-x, 0])
+        if tmr >= 800:
+            screen.blit(bg_img, [1600 - x, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
