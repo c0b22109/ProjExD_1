20fa67e47871cc435aed47eb1dc63196df294378
C0B22109 <c0b2210962@edu.teu.ac.jp>
2025-09-23 15:55:14 +0900

    練習問題8 背景画像が連続的になるように変更

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img_fliped = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
@@ -20,7 +21,7 @@ def main():
 
         screen.blit(bg_img, [-x, 0])
         if tmr >= 800:
-            screen.blit(bg_img, [1600 - x, 0])
+            screen.blit(bg_img_fliped, [1600 - x, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
